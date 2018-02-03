7cf2f28db3f067d99138721b5029b0fe0b56da30
phil <phild@protonmail.com>
2018-02-03 13:13:57 +0100

    training is saving training params and creating archive

--- experiments/training.py
+++ experiments/training.py
@@ -1,4 +1,9 @@
 # os.environ['CUDA_VISIBLE_DEVICES'] = '1'
+import os
+
+import time
+
+import numpy as np
 
 from workdir import work_dir
 
@@ -7,12 +12,14 @@ from fileaccess.GateGenerator import GateGenerator
 from models.Yolo.Yolo import Yolo
 from fileaccess.utils import save
 from backend.training import fit_generator
+from models.Yolo.TinyYolo import TinyYolo
 
 BATCH_SIZE = 8
 
-yolo = Yolo(batch_size=BATCH_SIZE, class_names=['gate'])
-
-trainset = GateGenerator(directory='resource/samples/mult_gate', batch_size=BATCH_SIZE, valid_frac=0.1)
+name = str(int(np.round(time.time() / 1000)))
+result_path = 'logs/' + name + '/'
+image_source = 'resource/samples/mult_gate_train'
+augment_active = False
 train_params = {'optimizer': 'adam',
                 'lr': 0.001,
                 'beta_1': 0.9,
@@ -20,8 +27,29 @@ train_params = {'optimizer': 'adam',
                 'epsilon': 1e-08,
                 'decay': 0.0005}
 
-yolo.compile(train_params)
+model = TinyYolo(batch_size=BATCH_SIZE, class_names=['gate'])
+# model = Yolo(batch_size=BATCH_SIZE, class_names=['gate'])
 
-training_history = fit_generator(yolo, trainset, out_file=result_path + 'yolo-gate-adam.h5', batch_size=BATCH_SIZE,
+if not os.path.exists(result_path):
+    os.makedirs(result_path)
+
+model.preprocessor.augment_active = augment_active
+
+trainset = GateGenerator(directory=image_source, batch_size=BATCH_SIZE, valid_frac=0.1, n_samples=32)
+
+model.compile(train_params)
+
+training_history = fit_generator(model, trainset, out_file=result_path + 'yolo-gate-adam.h5', batch_size=BATCH_SIZE,
                                  initial_epoch=0)
 save(training_history, 'training_history.pkl', result_path)
+
+exp_params = {'model': model.__class__.__name__,
+              'train_params': train_params,
+              'image_source': image_source,
+              'batch_size': BATCH_SIZE,
+              'n_samples': 1,
+              'augmentation': augment_active}
+
+save(exp_params, 'training_params.pkl', result_path)
+
+os.system('tar -cvf logs/' + name + '.tar.gz ' + result_path)
