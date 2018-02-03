c209c54202da47b16e8dcf20c289527532f9e69d
phil <phild@protonmail.com>
2018-02-03 12:49:21 +0100

    renamed

---
+++ experiments/evalset.py
@@ -0,0 +1,64 @@
+import os
+
+from evaluation.ConfidenceEvaluator import ConfidenceEvaluator
+from evaluation.MetricDetection import MetricDetection
+from workdir import work_dir
+
+work_dir()
+
+from models.Yolo.Yolo import Yolo
+from fileaccess.GateGenerator import GateGenerator
+from fileaccess.utils import save
+
+name = 'set06'
+
+# Image Source
+BATCH_SIZE = 100
+n_batches = 10
+image_source = 'resource/samples/mult_gate_valid/'
+color_format = 'bgr'
+
+# Model
+conf_thresh = 0
+weight_file = 'logs/yolo-noaug/yolo-gate-adam.h5'
+model = Yolo(class_names=['gate'], weight_file=weight_file, conf_thresh=conf_thresh)
+
+# Evaluator
+iou_thresh = 0.4
+
+# Result Paths
+result_path = 'logs/yolo-noaug/' + name + '/'
+result_file = 'result_' + name
+result_img_path = result_path + 'images_' + name + '/'
+exp_param_file = 'experiment_parameters_' + name + '.pkl'
+
+
+if not os.path.exists(result_path):
+    os.makedirs(result_path)
+
+if not os.path.exists(result_img_path):
+    os.makedirs(result_img_path)
+
+generator = GateGenerator(directory=image_source, batch_size=BATCH_SIZE, img_format='jpg',
+                          shuffle=True, color_format=color_format)
+
+evaluator = ConfidenceEvaluator(model, metrics=[MetricDetection(iou_thresh=iou_thresh)],
+                                out_file=result_path + result_file)
+
+evaluator.evaluate_generator(generator, n_batches=n_batches)
+
+exp_params = {'name': name,
+              'model': model.__class__.__name__,
+              'conf_thresh': conf_thresh,
+              'iou_thresh': iou_thresh,
+              'weight_file': weight_file,
+              'image_source': image_source,
+              'color_format': color_format,
+              'n_samples': n_batches * BATCH_SIZE}
+
+save(exp_params, exp_param_file, result_path)
+
+
+
+
+
--- experiments/evalstream.py
+++ experiments/evalstream.py
@@ -1,6 +1,5 @@
 import os
 
-
 from workdir import work_dir
 
 work_dir()
@@ -11,24 +10,31 @@ from evaluation.MetricOneGate import MetricOneGate
 from evaluation.BasicEvaluator import BasicEvaluator
 from fileaccess.utils import save
 
+name = 'stream2'
+
 # Image Source
 BATCH_SIZE = 100
 n_batches = 6
 image_source = 'resource/samples/stream_valid2/'
 color_format = 'bgr'
 
-# Result Paths
-result_path = 'logs/yolo-noaug/'
-result_file = 'test-stream2-iou0.4.pkl'
-result_img_path = result_path + '/stream/2/'
-
 # Model
 conf_thresh = 0.3
-weight_file = 'logs/yolo-nocolor/yolo-gate-adam.h5'
+weight_file = 'logs/yolo-noaug/yolo-gate-adam.h5'
 model = Yolo(class_names=['gate'], weight_file=weight_file, conf_thresh=conf_thresh)
 
 # Evaluator
 iou_thresh = 0.4
+
+# Result Paths
+result_path = 'logs/yolo-noaug/' + name + '/'
+result_file = 'result_' + name
+result_img_path = result_path + 'images_' + name + '/'
+exp_param_file = 'experiment_parameters_' + name + '.pkl'
+
+
+if not os.path.exists(result_path):
+    os.makedirs(result_path)
 
 if not os.path.exists(result_img_path):
     os.makedirs(result_img_path)
@@ -42,7 +48,8 @@ evaluator = BasicEvaluator(model,
 
 evaluator.evaluate_generator(generator, n_batches=n_batches)
 
-exp_params = {'model': model.__class__.__name__,
+exp_params = {'name': name,
+              'model': model.__class__.__name__,
               'conf_thresh': conf_thresh,
               'iou_thresh': iou_thresh,
               'weight_file': weight_file,
@@ -50,4 +57,4 @@ exp_params = {'model': model.__class__.__name__,
               'color_format': color_format,
               'n_samples': n_batches * BATCH_SIZE}
 
-save(exp_params, 'experiment_parameters.pkl', result_path)
+save(exp_params, exp_param_file, result_path)
