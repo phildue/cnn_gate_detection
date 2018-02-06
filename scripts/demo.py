2f4121ccb778dd5d575104109d6f29bbddb24ae8
phil <phild@protonmail.com>
2018-02-06 07:35:44 +0100

    SSD non max seems working

--- scripts/demo.py
+++ scripts/demo.py
@@ -11,6 +11,6 @@ from visualization.utils import demo_generator
 generator = GateGenerator(directory='resource/samples/stream_valid1/', batch_size=8, color_format='bgr',
                           shuffle=False, start_idx=400)
 
-model = Yolo(class_names=['gate'], weight_file='logs/1517854/yolo.h5', conf_thresh=0.6)
+model = Yolo(class_names=['gate'], weight_file='logs/1517854/yolo-gate-adam.h5', conf_thresh=0.6)
 
 demo_generator(model, generator, t_show=1)
