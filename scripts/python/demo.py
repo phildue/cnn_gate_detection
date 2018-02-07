d7bb8fc3ca44c083cf6edc979732de26de809b1d
phil <phild@protonmail.com>
2018-02-07 16:54:44 +0100

    cleanup

--- scripts/python/demo.py
+++ scripts/python/demo.py
@@ -1,3 +1,4 @@
+from models.ssd.SSD import SSD
 from models.yolo.TinyYolo import TinyYolo
 
 from workdir import work_dir
@@ -11,6 +12,6 @@ from visualization.utils import demo_generator
 generator = GateGenerator(directory='resource/samples/stream_valid1/', batch_size=8, color_format='bgr',
                           shuffle=False, start_idx=400)
 
-model = TinyYolo(class_names=['gate'], weight_file='logs/tinyyolo-noaug/yolo-gate-adam.h5', conf_thresh=0.6)
+model = SSD(img_shape=(416, 416, 3), n_classes=1, weight_file='logs/ssd7/SSD7.h5')
 
 demo_generator(model, generator, t_show=0)
