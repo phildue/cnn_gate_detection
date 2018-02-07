ae30741b2ead27fb7f6f6714c22e15afe53bc920
phil <phild@protonmail.com>
2018-02-07 08:16:17 +0100

    improved compile api, saving real train config

--- scripts/training.py
+++ scripts/training.py
@@ -20,37 +20,34 @@ BATCH_SIZE = 8
 name = str(int(np.round(time.time() / 1000)))
 result_path = 'logs/' + name + '/'
 image_source = 'resource/samples/mult_gate'
-augment_active = False
-train_params = {'optimizer': 'adam',
-                'lr': 0.001,
-                'beta_1': 0.9,
-                'beta_2': 0.999,
-                'epsilon': 1e-08,
-                'decay': 0.0005}
+augmenter = None
+
 
 # model = TinyYolo(batch_size=BATCH_SIZE, class_names=['gate'])
 # model = Yolo(batch_size=BATCH_SIZE, class_names=['gate'])
-model = SSD((416, 416, 3), 1)
+predictor = SSD((416, 416, 3), 1)
 if not os.path.exists(result_path):
     os.makedirs(result_path)
 
-model.preprocessor.augment_active = augment_active
+model_name = predictor.model.__class__.__name__
 
-trainset = GateGenerator(directory=image_source, batch_size=BATCH_SIZE, valid_frac=0.1)
+predictor.preprocessor.augmenter = augmenter
 
-model.compile(train_params)
+data_generator = GateGenerator(directory=image_source, batch_size=BATCH_SIZE, valid_frac=0.1)
 
-training_history = fit_generator(model, trainset, out_file='yolo-gate-adam.h5', batch_size=BATCH_SIZE,
+predictor.compile(None)
+
+training_history = fit_generator(predictor, data_generator, out_file=model_name + '.h5', batch_size=BATCH_SIZE,
                                  initial_epoch=0, log_dir=result_path)
 save(training_history, 'training_history.pkl', result_path)
 
-exp_params = {'model': model.__class__.__name__,
-              'train_params': train_params,
+exp_params = {'model': model_name,
+              'train_params': predictor.model.train_params,
               'image_source': image_source,
               'batch_size': BATCH_SIZE,
-              'n_samples': 1,
-              'augmentation': augment_active}
+              'n_samples': data_generator.n_samples,
+              'augmentation': predictor.preprocessor.augmenter.__class__.__name__}
 
-save(exp_params, 'training_params.pkl', result_path)
+save(exp_params, 'training_params.txt', result_path)
 
 os.system('tar -cvf logs/' + name + '.tar.gz ' + result_path)
