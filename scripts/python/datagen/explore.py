b91815b11c85cf8913759b789ae39268d5e6f1d4
phil <phild@protonmail.com>
2018-02-09 16:33:32 +0100

    point projector can assign class name for gates, sample generation with new gate working

--- scripts/python/datagen/explore.py
+++ scripts/python/datagen/explore.py
@@ -2,13 +2,13 @@ import os
 import sys
 
 import numpy as np
-from shotgen.engine3d.opengl.GateGL2016Tall import GateGL2016Tall
+from shotgen.engine3d.opengl.GateGLTall import GateGLTall
 
-from shotgen.engine3d.opengl.GateGL250 import GateGL250
+from shotgen.engine3d.opengl.GateGLThin250 import GateGLThin250
 
 from workdir import work_dir
 
-from shotgen.engine3d.opengl.GateGL2016Large import GateGL2016Large
+from shotgen.engine3d.opengl.GateGLLarge import GateGLThickLarge
 
 from fileaccess.SetFileParser import SetFileParser
 from labels.Pose import Pose
@@ -20,7 +20,7 @@ from shotgen.positiongen.RandomPositionGen import RandomPositionGen
 work_dir()
 
 from scene.Camera import Camera
-from scene.Gate250 import Gate250
+from scene.GateThin250 import Gate250
 from scene.Light import Light
 from scene.Scene import Scene
 from shotgen.engine3d.SceneEngine import SceneEngine
@@ -32,18 +32,16 @@ cam = Camera(1000, init_pose=Pose(dist_forward=5))
 gate_path = "resource/gates/"
 gate_file = "gate250.obj"
 
-gate_1 = (GateGL2016Tall(), Pose())
-gate_2 = (
-    GateGL2016Large(), Pose(dist_side=-2.0, dist_forward=2.0, yaw=np.pi / 4))
+gate_1 = (GateGLOpen(), Pose().to_scene_unit)
+gate_2 = (GateGLThickLarge(), Pose(dist_side=-2.0, dist_forward=2.0, yaw=np.pi / 4).to_scene_unit)
 
+gate_generator = GateGen(gates=[GateGLThickLarge(), GateGLTall()], n_gate_range=(3, 8))
 
-gate_generator = GateGen(gate_path=gate_path, gate_file=gate_file, n_gate_range=(3, 4))
+# gates = [gate_1]
+gates = gate_generator.generate()
+scene_engine = SceneEngine(Scene(cam, objects=gates, lights=[Light((-2, 2, 3)), Light((0, 2, 4)), Light((-2, 0, 3))]))
 
-gates = [gate_1]
-# gates = gate_generator.generate()
-scene_engine = SceneEngine(Scene(cam, objects=gates,lights=[Light((-2, 2, 3)), Light((0, 2, 4)), Light((-2, 0, 3))]))
-
-cam_range_side = (-0.5, 0.5)
+cam_range_side = (-1, 1)
 cam_range_forward = (0, 10)
 cam_range_lift = (-0.5, 1.0)
 cam_range_pitch = (-np.pi / 4, np.pi / 4)
@@ -62,7 +60,7 @@ position_gen = RandomPositionGen(range_dist_side=cam_range_side,
 shot_path = 'samplegen/resource/shots/stream_recorded/'
 if not os.path.exists(shot_path):
     os.makedirs(shot_path)
-recorder = None # SetFileParser(shot_path, img_format='bmp', label_format='pkl', start_idx=0)
+recorder = None  # SetFileParser(shot_path, img_format='bmp', label_format='pkl', start_idx=0)
 explorer = Explorer(scene_engine, position_gen=position_gen, recorder=recorder)
 explorer.event_loop()
 # save(explorer.trajectory, 'recorded_trajectroy.pkl', shot_path)
