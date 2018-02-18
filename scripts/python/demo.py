b4f6aeefa5395ec4efaa0062a321c195d24804b4
phil <phild@protonmail.com>
2018-02-18 14:04:15 +0100

    added some comments

--- scripts/python/demo.py
+++ scripts/python/demo.py
@@ -1,6 +1,5 @@
 from fileaccess.GateGenerator import GateGenerator
-
-from frontend.models import Yolo
+from frontend.models.yolo.Yolo import Yolo
 
 from workdir import work_dir
 
@@ -8,14 +7,14 @@ work_dir()
 
 from frontend.visualization.utils import demo_generator
 
-generator = GateGenerator(directory='resource/samples/cyberzoo_conv/', batch_size=8, color_format='bgr',
-                          shuffle=True, start_idx=0)
+generator = GateGenerator(directory='resource/samples/stream_valid2/', batch_size=8, color_format='bgr',
+                          shuffle=False, start_idx=500)
 
 # generator = VocGenerator("resource/backgrounds/VOCdevkit/VOC2012/Annotations/",
 #                          "resource/backgrounds/VOCdevkit/VOC2012/JPEGImages/", batch_size=8, color_format='bgr')
 
 # model = SSD.ssd300(n_classes=20, weight_file='logs/ssd300/SSD300.h5', conf_thresh=0.01, color_format='bgr')
-model = Yolo.tiny_yolo(class_names=['gate'], conf_thresh=0.3, color_format='yuv',
-                       weight_file='logs/tinyyolo_10k/TinyYolo.h5')
+model = Yolo.yolo_v2(class_names=['gate'], conf_thresh=0.1, color_format='yuv',
+                     weight_file='logs/yolov2_25k/YoloV2.h5')
 
 demo_generator(model, generator, t_show=0)
