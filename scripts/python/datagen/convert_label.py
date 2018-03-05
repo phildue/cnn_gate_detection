262245699ba269fdf10d0cbb16681000cd78c5d5
phil <phild@protonmail.com>
2018-03-05 13:15:19 +0100

    convert update

--- scripts/python/datagen/convert_label.py
+++ scripts/python/datagen/convert_label.py
@@ -1,9 +1,16 @@
+from labels.ObjectLabel import ObjectLabel
+
 from fileaccess.LabelFileParser import LabelFileParser
 
 from workdir import work_dir
 
 work_dir()
 
-parser = LabelFileParser('resource/samples/mult_gate_aligned', '.pkl')
+parser = LabelFileParser('resource/samples/mult_gate_aligned_blur_distort', 'pkl')
 labels = parser.read()
+
+ObjectLabel.classes = 'gate'
+for l in labels:
+    for o in l.objects:
+        o.class_name = 'gate'
 parser.write(labels)
