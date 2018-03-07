900c4f6286737db704615fbee9769590edd45d16
phil <phild@protonmail.com>
2018-03-07 18:25:59 +0100

    fit_generator can now deactivate callbacks

--- src/python/modelzoo/backend/tensor/training.py
+++ src/python/modelzoo/backend/tensor/training.py
@@ -6,12 +6,21 @@ from utils.fileaccess.DatasetGenerator import DatasetGenerator
 
 def fit_generator(predictor: Predictor, dataset_gen: DatasetGenerator, out_file, batch_size, initial_epoch=0,
                   epochs=100, patience=3, log_dir='./logs'):
-    early_stop = EarlyStopping(monitor='val_loss', min_delta=0.001, patience=patience, mode='min', verbose=1)
-    checkpoint = ModelCheckpoint(log_dir + out_file, monitor='val_loss', verbose=2, save_best_only=True,
-                                 mode='min', save_weights_only=True,
-                                 period=1)
-    tensorboard = TensorBoard(batch_size=batch_size, log_dir=log_dir, write_images=True, histogram_freq=0)
+    callbacks = []
+    if patience > -1:
+        early_stop = EarlyStopping(monitor='val_loss', min_delta=0.001, patience=patience, mode='min', verbose=1)
+        callbacks.append(early_stop)
+    if out_file is not None:
+        checkpoint = ModelCheckpoint(log_dir + out_file, monitor='val_loss', verbose=2, save_best_only=True,
+                                     mode='min', save_weights_only=False,
+                                     period=1)
+        callbacks.append(checkpoint)
+    if log_dir is not None:
+        tensorboard = TensorBoard(batch_size=batch_size, log_dir=log_dir, write_images=True, histogram_freq=0)
+        callbacks.append(tensorboard)
+
     history = History()
+    callbacks.append(history)
 
     predictor.net.backend.fit_generator(
         generator=predictor.preprocessor.preprocess_train_generator(dataset_gen.generate()),
@@ -21,6 +30,6 @@ def fit_generator(predictor: Predictor, dataset_gen: DatasetGenerator, out_file,
         verbose=1,
         validation_data=predictor.preprocessor.preprocess_train_generator(dataset_gen.generate_valid()),
         validation_steps=100,
-        callbacks=[early_stop, checkpoint, tensorboard, history])
+        callbacks=callbacks)
 
     return history.history
