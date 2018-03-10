f086ba0ccff4edb072ce1327116f21af1655bf47
phil <phild@protonmail.com>
2018-03-10 10:50:44 +0100

    barrel distortion working

--- src/python/test/fisheye.py
+++ src/python/test/fisheye.py
@@ -1,15 +1,10 @@
 import random
 
-from utils.fileaccess.GateGenerator import GateGenerator
-from utils.imageprocessing.BarrelDistortion import BarrelDistortion
-
 from utils.fileaccess.VocGenerator import VocGenerator
-from utils.imageprocessing.Backend import resize, imread
-from utils.imageprocessing.Image import Image
+from utils.imageprocessing.Backend import resize
+from utils.imageprocessing.BarrelDistortion import BarrelDistortion
 from utils.imageprocessing.Imageprocessing import show
-from utils.imageprocessing.fisheye import fisheye
 from utils.workdir import work_dir
-import numpy as np
 
 work_dir()
 # dataset = GateGenerator(directories=['resource/samples/cyberzoo_conv/'],
@@ -18,29 +13,15 @@ work_dir()
 #                         label_format=None).generate()
 dataset = VocGenerator(batch_size=100).generate()
 batch = next(dataset)
-N = 5
-idx = random.randint(0, 99 - N)
 
-img = batch[idx][0]
-label = batch[idx][1]
-img, label = resize(img, (600, 600), label=label)
+distortion_model = BarrelDistortion((600, 600), [.7, 0], use_newton_approx=True)
 
-img = imread('resource/samples/fisheye.jpg', 'bgr')
-test_mat = np.zeros((300, 300, 3))
-for i in range(0, 300, 10):
-    test_mat[i, :] = (255, 255, 255)
-for j in range(0, 300, 10):
-    test_mat[:, j] = (255, 255, 255)
-test_mat = 255 - test_mat
-img = Image(test_mat, 'bgr')
-distortion_model = BarrelDistortion(img.shape[:2], [.2, 0], max_iterations=100, distortion_radius=1.5)
-show(img, labels=label, t=1)
+for img, label, _ in batch:
+    img, label = resize(img, (600, 600), label=label)
 
-img_ud, label_ud = distortion_model.undistort(img, label)
-img_d, label_d = distortion_model.distort(img, label)
-img_rd, label_rd = distortion_model.undistort(img_d, label)
+    show(img, labels=label, t=1)
+    img_d, label_d = distortion_model.distort(img, label)
+    img_ud, label_ud = distortion_model.undistort(img_d, label_d)
 
-show(img_ud, labels=label_ud, name='undistort', t=1)
-
-show(img_d, labels=label_d, name='distort', t=1)
-show(img_rd, labels=label_rd, name='restored')
+    show(img_d, labels=label_d, name='distort', t=1)
+    show(img_ud, labels=label_ud, name='restored')
--- src/python/utils/imageprocessing/BarrelDistortion.py
+++ src/python/utils/imageprocessing/BarrelDistortion.py
@@ -1,5 +1,4 @@
 import numpy as np
-from cv2 import morphologyEx, MORPH_CLOSE
 
 from utils.imageprocessing.Image import Image
 from utils.labels.GateCorners import GateCorners
@@ -10,7 +9,23 @@ from utils.timing import tic, toc
 
 class BarrelDistortion:
     def __init__(self, img_shape, rad_dist_params, squeeze=1.0, non_rad_dist_params=(0, 0),
-                 max_iterations=100, distortion_radius=1.0, center=None, conv_thresh=0.01):
+                 max_iterations=100, distortion_radius=1.0, center=None, conv_thresh=0.01, scale=1.0):
+        """
+        Barrel Distortion model.
+        [Vass, G., & Perlaki, T. (n.d.). Applying and removing lens distortion in post production.
+        Retrieved from http://www.vassg.hu/pdf/vass_gg_2003_lo.pdf]
+
+        :param img_shape: original image shape
+        :param rad_dist_params: parameters for radial distortion
+        :param squeeze: parameter for squeeze effect
+        :param non_rad_dist_params: parameters for non-radial distortion (yshift, xshift)
+        :param distortion_radius: radius on which distortion should be applied
+        :param center: center around which distortion is applied
+        :param scale: scale distortion
+        :param conv_thresh: convergence threshold for newton approximation
+        :param max_iterations: maximum iterations for newton approximation
+        """
+        self.scale = scale
         self.img_shape = img_shape
         self.epsilon = conv_thresh
         self.center = center
@@ -19,27 +34,96 @@ class BarrelDistortion:
         self.max_iterations = max_iterations
         self.squeeze = squeeze
         self.rad_dist_params = rad_dist_params
-        self.mapping_undist, self.mapping_dist = self.create_mapping()
+        self.mapping_undist, self.mapping_dist = self._create_mapping()
 
-    def create_mapping(self):
+    def undistort(self, img: Image, label: ImgLabel = None):
+        """
+        Remove distortion from an image
+        :param img: image
+        :param label: label
+        :return: img,label without distortion
+        """
+        mat = img.array
+        mat_undistorted = self._distort_mat(mat, self.mapping_dist)
+        if label is not None:
+            label_undistorted = self._distort_label(label, self.mapping_undist)
+            return Image(mat_undistorted, img.format), label_undistorted
+        else:
+            return Image(mat_undistorted, img.format)
+
+    def distort(self, img: Image, label: ImgLabel = None):
+        """
+        Apply distortion to an image
+        :param img: image
+        :param label:  label
+        :return: img, label with distortion
+        """
+        mat = img.array
+        mat_undistorted = self._distort_mat(mat, self.mapping_undist)
+        if label is not None:
+            label_distorted = self._distort_label(label, self.mapping_dist)
+            return Image(mat_undistorted, img.format), label_distorted
+        else:
+            return Image(mat_undistorted, img.format)
+
+    @staticmethod
+    def _distort_label(label: ImgLabel, mapping):
+        """
+        Move the coordinates of the label according to distortion.
+        :param label: image label
+        :param mapping: mapping containing new pixel coordinates given the old coordinates
+        :return: distorted label
+        """
+        label_distorted = label.copy()
+        h, w = mapping.shape[:2]
+        for obj in label_distorted.objects:
+            y_min = np.max([obj.y_min, 1])
+            x_min = np.max([obj.x_min, 1])
+            y_max = np.min([obj.y_max, h - 1])
+            x_max = np.min([obj.x_max, w - 1])
+            x_min_d, y_min_d = mapping[h - int(y_min), int(x_min)]
+            x_max_d, y_max_d = mapping[h - int(y_max), int(x_max)]
+            obj.x_min = np.min([x_min_d, x_max_d])
+            obj.x_max = np.max([x_min_d, x_max_d])
+            obj.y_min = h - np.min([y_min_d, y_max_d])
+            obj.y_max = h - np.max([y_min_d, y_max_d])
+            if isinstance(obj, GateLabel):
+                corners = obj.gate_corners.as_mat
+                corners_dist = np.zeros_like(corners)
+                for i in range(corners.shape[0]):
+                    corners_dist[i] = mapping[int(corners[i, 1]), int(corners[i, 0])]
+                obj.gate_corners = GateCorners.from_mat(corners_dist)
+
+        return label_distorted
+
+    def _create_mapping(self):
+        """
+        Creates mapping to apply and remove distortion.
+        :return:  mapping to apply and remove distortion
+        """
         h, w = self.img_shape
         x = np.tile(np.arange(0, w), (h, 1))
         y = np.tile(np.reshape(np.arange(0, h), (h, 1)), (1, w))
         coords = np.concatenate((np.expand_dims(x, -1), np.expand_dims(y, -1)), -1)
         coords_norm = self._normalize(coords.astype(np.float))
-        mapping_undist = self._distortion_model(coords_norm)
+        mapping_undist = self.scale * self._distortion_model(coords_norm)
         mapping_dist = 2 * coords_norm - mapping_undist
-        # mapping_dist = self._inverse_model(coords_norm,mapping_dist)
+        mapping_dist = self.scale * self._inverse_model_approx(coords_norm, mapping_dist)
         mapping_undist = self._denormalize(mapping_undist)
         mapping_dist = self._denormalize(mapping_dist)
-        return mapping_undist.astype(np.uint), mapping_dist.astype(np.uint)
+        return mapping_undist, mapping_dist
 
-    def _distortion_model(self, mat: np.array):
+    def _distortion_model(self, coord: np.array):
+        """
+        Model for distortion.:
+        :param coord: distorted pixel coordinates
+        :return: undistored pixel coordinates
+        """
         k_1, k_2 = self.rad_dist_params
         l_y, l_x = self.non_rad_dist_params
         s = self.squeeze
-        x = mat[:, :, 0]
-        y = mat[:, :, 1]
+        x = coord[:, :, 0]
+        y = coord[:, :, 1]
 
         x_d = x * (1 + k_1 * x ** 2 + k_1 * (1 + l_x) * y ** 2 + k_2 * (x ** 2 + y ** 2) ** 2)
         y_d = y * (1 + k_1 / s * x ** 2 + k_1 / s * (1 + l_y) * y ** 2 + k_2 / s * (x ** 2 + y ** 2) ** 2)
@@ -47,103 +131,95 @@ class BarrelDistortion:
 
         return mat_d
 
-    def _normalize(self, mat: np.array):
+    def _normalize(self, coord: np.array):
         """
         Normalizes the pixel coordinates so that the origin is center and the radius is distortion_radius.
-        :param mat: coordinate matrix
+        :param coord: coordinate matrix
         :return: normalize coordinate matrix
         """
-        h, w = mat.shape[:2]
+        h, w = coord.shape[:2]
 
         center = self.center if self.center is not None else np.array([w, h]) / 2
 
         h /= self.distortion_radius
         w /= self.distortion_radius
 
-        mat_dimension_less = (mat - center) / np.linalg.norm(np.array([w, h]) / 2)
+        mat_dimension_less = (coord - center) / np.linalg.norm(np.array([w, h]) / 2)
         return mat_dimension_less
 
-    def _denormalize(self, mat: np.array):
-
-        h, w = mat.shape[:2]
+    def _denormalize(self, coord: np.array):
+        """
+        Scales the normalized pixel coordinates back to original size.
+        :param coord: normalized pixel coordinates
+        :return: denormalized coordinates
+        """
+        h, w = coord.shape[:2]
         center = self.center if self.center is not None else np.array([w, h]) / 2
 
         h /= self.distortion_radius
         w /= self.distortion_radius
 
-        mat_denorm = mat * np.linalg.norm(np.array([w, h]) / 2) + center
+        mat_denorm = coord * np.linalg.norm(np.array([w, h]) / 2) + center
 
         return mat_denorm
 
-    def undistort(self, img: Image, label: ImgLabel = None):
-        mat = img.array
-        mat_undistorted = self._undistort_mat(mat)
-        # mat_undistorted = self._fill_holes(mat_undistorted)
-        # label_distorted = self._distort_label(label, center)
+    @staticmethod
+    def _bilinear_interp(mat: np.array, x, y):
+        """
+        Approximates f(x,y) using f(0,0),f(0,1),f(1,0),f(1,1) that is
+        the new image given the 4 neighbouring pixels of the source image
+        :param mat: source image
+        :param x: source x
+        :param y: source y
+        :return: interpolated new pixel
+        """
+        channels = mat.shape[-1]
+        x0 = int(np.floor(x))
+        y0 = int(np.floor(y))
+        x1 = int(np.ceil(x))
+        y1 = int(np.ceil(y))
+        f00 = mat[y0, x0]
+        f01 = mat[y1, x0]
+        f10 = mat[y0, x1]
+        f11 = mat[y1, x1]
+        f = np.array([[f00, f01],
+                      [f10, f11]])
 
-        return Image(mat_undistorted, img.format), label
+        y -= float(y0)
+        x -= float(x0)
+        y_vec = np.array([[1 - y], [y]])
+        x_vec = np.array([[1 - x, x]])
+        out = np.zeros((channels,), dtype=np.int8)
+        for i in range(channels):
+            out[i] = (x_vec.dot(f[:, :, i].dot(y_vec))).astype(np.int8)
 
-    def _fill_holes(self, mat):
-        h, w = self.img_shape
-        mat_filled = mat.copy()
-        iterations = 8
-        for i in range(1, h - 1):
-            for j in range(1, w - 1):
-                for _ in range(iterations):
-                    if np.all(mat_filled[i, j] == 0):
-                        mat_filled[i, j, 0] = np.mean(mat_filled[i - 1:i + 1, j - 1:j + 1, 0])
-                        mat_filled[i, j, 1] = np.mean(mat_filled[i - 1:i + 1, j - 1:j + 1, 1])
-                        mat_filled[i, j, 2] = np.mean(mat_filled[i - 1:i + 1, j - 1:j + 1, 2])
-        return mat_filled
+        return out.T
 
-    def distort(self, img: Image, label: ImgLabel = None):
-        mat = img.array
-        mat_undistorted = self._distort_mat(mat)
-        # mat_undistorted = morphologyEx(mat_undistorted, MORPH_CLOSE, (3, 3), iterations=2)
-
-        return Image(mat_undistorted, img.format), label
-
-    def _undistort_mat(self, mat):
-
-        h, w = self.img_shape
-        mat_undistorted = np.zeros_like(mat)
-        for i in range(h):
-            for j in range(w):
-                if 0 <= self.mapping_undist[i, j][1] < h and 0 <= self.mapping_undist[i, j][0] < w:
-                    mat_undistorted[self.mapping_undist[i, j][1], self.mapping_undist[i, j][0]] = mat[i, j]
-
-        return mat_undistorted
-
-    def _distort_mat(self, mat):
-
+    def _distort_mat(self, mat: np.array, mapping: np.array):
+        """
+        Distort the image with bilinear interpolation.
+        :param mat: image
+        :param mapping: mapping containing the old indeces given the new coordinates
+        :return: distorted image
+        """
         h, w = self.img_shape
         mat_distorted = np.zeros_like(mat)
         for i in range(h):
             for j in range(w):
-                if 0 <= self.mapping_dist[i, j][1] < h and 0 <= self.mapping_dist[i, j][0] < w:
-                    mat_distorted[self.mapping_dist[i, j][1], self.mapping_dist[i, j][0]] = mat[i, j]
+                x, y = mapping[i, j]
+                if 1 < x < float(w) - 1 and 1 < y < float(h) - 1:
+                    mat_distorted[i, j] = self._bilinear_interp(mat, x, y)
 
         return mat_distorted
 
-    def _distort_label(self, label: ImgLabel, center):
-        label_distorted = label.copy()
-        for obj in label_distorted.objects:
-            x_min_d, y_min_d = self.distort_point(np.array(obj.x_min, obj.y_min), self.k, center)
-            x_max_d, y_max_d = self.distort_point(np.array(obj.x_max, obj.y_max), self.k, center)
-            obj.x_min = x_min_d
-            obj.x_max = x_max_d
-            obj.y_min = y_min_d
-            obj.y_max = y_max_d
-            if isinstance(obj, GateLabel):
-                corners = obj.gate_corners.as_mat
-                corners_dist = np.apply_along_axis(self.distort_point, 0, corners,
-                                                   kwargs={'k': self.k, 'center': center})
-                obj.gate_corners = GateCorners.from_mat(corners_dist)
-
-        return label_distorted
-
-    def _inverse_model(self, mat: np.array, init):
-        h, w = mat.shape[:2]
+    def _inverse_model_approx(self, coord: np.array, init):
+        """
+        Approximates the inverted model to apply distortion with newton method
+        :param coord: pixel coordinates
+        :param init: initial guess for mapping
+        :return: approximated pixel coordinates after distortion
+        """
+        h, w = coord.shape[:2]
         mat_cur = init.copy().astype(np.float)
         for t in range(self.max_iterations):
             tic()
@@ -155,8 +231,8 @@ class BarrelDistortion:
                     grad = gradient[i, j]
                     np.fill_diagonal(grad, np.diag(grad) + 0.000001)
                     prev = np.reshape(mat_prev[i, j], (2, 1))
-                    diff = np.reshape((dist_model[i, j] - mat[i, j]), (2, 1))
-                    update = prev - 0.1 * np.dot(np.linalg.inv(grad), diff)
+                    diff = np.reshape((dist_model[i, j] - coord[i, j]), (2, 1))
+                    update = prev - 0.1 * np.linalg.inv(grad).dot(diff)
                     mat_cur[i, j] = update.flatten()
             delta = np.linalg.norm(mat_cur - mat_prev)
             if delta < self.epsilon:
@@ -164,14 +240,19 @@ class BarrelDistortion:
             toc('iteration: {} - delta: {} - time: '.format(t, np.round(delta, 2)))
         return mat_cur
 
-    def _gradient(self, mat):
+    def _gradient(self, coord: np.array):
+        """
+        Gradient of distortion model
+        :param coord: pixel coordinates
+        :return: value of gradient
+        """
         k_1, k_2 = self.rad_dist_params
         l_y, l_x = self.non_rad_dist_params
         s = self.squeeze
 
-        x = mat[:, :, 0]
-        y = mat[:, :, 1]
-        h, w = mat.shape[:2]
+        x = coord[:, :, 0]
+        y = coord[:, :, 1]
+        h, w = coord.shape[:2]
 
         dx_x = k_1 * x ** 2 + k_1 * y ** 2 * (l_x + 1) + k_2 * (x ** 2 + y ** 2) ** 2 + x * (
             2 * k_1 * x + 4 * k_2 * x * (x ** 2 + y ** 2)) + 1
