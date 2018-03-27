d22ce72f64da326592b9cda78ed99ecc08111fd3
phildue <phild@protonmail.com>
2018-03-27 09:14:29 +0200

    v2 should work again

--- src/python/run/training/gate/v2_bebop.py
+++ src/python/run/training/gate/v2_bebop.py
@@ -18,15 +18,15 @@ batch_size = 4
 image_source = ["resource/ext/samples/bebop20k/"]
 test_image_source = ['resource/ext/samples/cyberzoo']
 max_epochs = 300
-n_samples = 25000
+n_samples = 8
 
 anchors = np.array([[0.13809687, 0.27954467],
                     [0.17897748, 0.56287585],
                     [0.36642611, 0.39084195],
                     [0.60043528, 0.67687858]])
 
-predictor = Yolo.tiny_yolo(norm=(160, 320), grid=(5, 10), class_names=['gate'], batch_size=batch_size,
-                           color_format='bgr', anchors=anchors)
+predictor = Yolo.yolo_V2(norm=(160, 320), grid=(5, 10), class_names=['gate'], batch_size=batch_size,
+                         color_format='bgr', anchors=anchors)
 data_generator = GateGenerator(image_source, batch_size=batch_size, valid_frac=0.1, n_samples=n_samples,
                                color_format='bgr', label_format='xml')
 test_gen = GateGenerator(test_image_source, batch_size=batch_size, valid_frac=0, color_format='bgr', label_format='xml')
@@ -62,7 +62,10 @@ def lr_schedule(epoch):
         return 0.000001
 
 
-test_metric = TestMetric(test_gen, ModelEvaluator(predictor, test_result, False))
+test_metric = TestMetric(test_gen,
+                         ModelEvaluator(predictor, verbose=False),
+                         ConfidenceEvaluator(predictor, metrics=[MetricDetection(show_=False)], out_file=test_result,
+                                             color_format='bgr'))
 training = Training(predictor, data_generator,
                     out_file=model_name + '.h5',
                     patience=-1,
