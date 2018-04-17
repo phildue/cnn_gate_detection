0dbc926daec903019e929b79fa09602b89ae486c
phildue <phild@protonmail.com>
2018-04-17 16:59:13 +0200

    added gatev0

--- src/python/modelzoo/models/gatenet/GateNet.py
+++ src/python/modelzoo/models/gatenet/GateNet.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from modelzoo.backend.tensor.gatenet.GateDetectionLoss import GateDetectionLoss
+from modelzoo.backend.tensor.gatenet.GateNetV0 import GateNetV0
 from modelzoo.backend.tensor.gatenet.GateNetV1 import GateNetV1
 from modelzoo.backend.tensor.gatenet.GateNetV2 import GateNetV2
 from modelzoo.models.Postprocessor import Postprocessor
@@ -16,6 +17,55 @@ class GateNet(Predictor):
     @property
     def input_shape(self):
         return self.norm[0], self.norm[1], 3
+
+    @staticmethod
+    def v0(norm=(416, 416),
+           grid=(13, 13),
+           anchors=None,
+           batch_size=8,
+           scale_noob=1.0,
+           scale_conf=5.0,
+           scale_coor=1.0,
+           scale_prob=1.0,
+           conf_thresh=0.3,
+           weight_file=None,
+           color_format='yuv',
+           augmenter: ImgTransform = None,
+           n_polygon=4):
+
+        if anchors is None:
+            anchors = np.array([[1.08, 1.19],
+                                [3.42, 4.41],
+                                [6.63, 11.38],
+                                [9.42, 5.11],
+                                [16.62, 10.52]])
+
+        n_boxes = anchors.shape[0]
+        loss = GateDetectionLoss(grid=grid,
+                                 n_boxes=n_boxes,
+                                 n_polygon=4,
+                                 weight_loc=scale_coor,
+                                 weight_conf=scale_conf,
+                                 weight_prob=scale_prob,
+                                 weight_noobj=scale_noob)
+
+        net = GateNetV0(loss=loss,
+                        anchors=anchors,
+                        img_shape=norm,
+                        grid=grid,
+                        weight_file=weight_file,
+                        n_boxes=n_boxes,
+                        n_polygon=n_polygon)
+
+        return GateNet(net,
+                       anchors=anchors,
+                       batch_size=batch_size,
+                       grid=grid,
+                       norm=norm,
+                       conf_thresh=conf_thresh,
+                       color_format=color_format,
+                       augmenter=augmenter,
+                       n_polygon=n_polygon)
 
     @staticmethod
     def v1(norm=(416, 416),
