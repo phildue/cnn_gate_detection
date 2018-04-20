d1bd81a33c7b695d0defaf4d5d8c1d4f5f53bb57
phildue <phild@protonmail.com>
2018-04-20 11:09:37 +0200

    added scripts to run ssd training

--- src/python/run/training/gate/ssd_industrial.py
+++ src/python/run/training/gate/ssd_industrial.py
@@ -1,2 +1,10 @@
-# ./train.py --logtostderr --train_dir=e:/doc/study/thesis/mavv/logs/ssd_industrial/ --pipeline_config_path=e
-#:/doc/study/thesis/mavv/logs/ssd_industrial/pipeline.config
+import os
+
+from utils.workdir import cd_work
+
+cd_work()
+
+os.system('./lib/tf-models/research/object/train.py '
+          '--logtostderr '
+          '--train_dir=logs/ssd_industrial/ '
+          '--pipeline_config_path=logs/ssd_industrial/pipeline.config')
