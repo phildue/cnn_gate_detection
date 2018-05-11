325cf06e677030d0591b4ace19e578daa5016ecf
phildue <phild@protonmail.com>
2018-05-11 15:09:03 +0200

    evalspeed is more flexible

--- src/python/run/evaluation/evalspeed.py
+++ src/python/run/evaluation/evalspeed.py
@@ -1,31 +1,64 @@
+import argparse
+
 from modelzoo.evaluation.SpeedEvaluator import SpeedEvaluator
 from modelzoo.models.gatenet.GateNet import GateNet
+from modelzoo.models.yolo.Yolo import Yolo
 from utils.fileaccess.GateGenerator import GateGenerator
 from utils.fileaccess.utils import create_dirs, save_file
 from utils.workdir import cd_work
 
+parser = argparse.ArgumentParser()
+parser.add_argument("model", help="model name",
+                    type=str)
+parser.add_argument("work_dir", help="Working directory", type=str)
+parser.add_argument("--image_source", help="List of folders to be scanned for test images", type=[str],
+                    default=['resource/ext/samples/daylight_test'])
+parser.add_argument("--batch_size", help="Batch Size", type=int, default=1)
+parser.add_argument("--n_batches", help="Amount of batches", type=int, default=None)
+args = parser.parse_args()
+
 cd_work()
 
 name = 'speed'
-work_dir = 'out/gatev8_mixed/'
+work_dir = args.work_dir
 """
 Image Source
 """
-batch_size = 20
-n_batches = 6
-image_source = ['resource/ext/samples/daylight_test']
+batch_size = args.batch_size
+image_source = args.image_source
 color_format = 'bgr'
 
 generator = GateGenerator(directories=image_source, batch_size=batch_size, img_format='jpg',
                           shuffle=True, color_format=color_format, label_format='xml')
+n_batches = args.n_batches if args.n_batches is not None else int(generator.n_samples / batch_size)
 
 """
 Model config
 """
-conf_thresh = 0.3
-weight_file = work_dir + 'model.h5'
-model = GateNet.v5(conf_thresh=conf_thresh, batch_size=batch_size)
-
+if args.model == "tiny_yolo":
+    model = Yolo.tiny_yolo(batch_size=batch_size)
+elif args.model == "yolo":
+    model = Yolo.yolo_v2(batch_size=batch_size)
+elif args.model == "gatev5":
+    model = GateNet.v5(batch_size=batch_size)
+elif args.model == "gatev6":
+    model = GateNet.v6(batch_size=batch_size)
+elif args.model == "gatev7":
+    model = GateNet.v7(batch_size=batch_size)
+elif args.model == "gatev8":
+    model = GateNet.v8(batch_size=batch_size)
+elif args.model == "gatev9":
+    model = GateNet.v9(batch_size=batch_size)
+elif args.model == "gatev10":
+    model = GateNet.v10(batch_size=batch_size)
+elif args.model == "gatev11":
+    model = GateNet.v11(batch_size=batch_size)
+elif args.model == "gatev12":
+    model = GateNet.v12(batch_size=batch_size)
+elif args.model == "gatev13":
+    model = GateNet.v13(batch_size=batch_size)
+else:
+    raise ValueError("Unknown model name!")
 """
 Evaluator
 """
@@ -43,8 +76,6 @@ evaluator.evaluate_generator(generator, n_batches=n_batches)
 exp_params = {'name': name,
               'model': model.net.__class__.__name__,
               'evaluator': evaluator.__class__.__name__,
-              'conf_thresh': conf_thresh,
-              'weight_file': weight_file,
               'image_source': image_source,
               'color_format': color_format,
               'n_samples': n_batches * batch_size}
