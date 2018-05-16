45b6c06a69546809ed49345ea62532ac14eb8f1f
phild <phild@protonmail.com>
2018-05-16 08:35:41 +0200

    modelconversion is working

--- src/python/run/etc/modelconv.py
+++ src/python/run/etc/modelconv.py
@@ -22,66 +22,13 @@ out_name = out_path + 'model'
 quantize = False
 num_outputs = 1
 
-
-
-
 """
 Prepare model
 """
+K.set_learning_phase(0)
 model = GateNet.v8(weight_file=in_file)
 
-K.set_learning_phase(0)
-
-input = Input((416, 416, 3))
-conv1 = Conv2D(16, kernel_size=(3, 3), strides=(1, 1), padding='same', use_bias=False)(input)
-norm1 = BatchNormalization()(conv1)
-act1 = LeakyReLU(alpha=0.1)(norm1)
-pool1 = MaxPooling2D((2, 2))(act1)
-# 208
-conv2 = Conv2D(32, kernel_size=(3, 3), strides=(1, 1), padding='same', use_bias=False)(pool1)
-norm2 = BatchNormalization()(conv2)
-act2 = LeakyReLU(alpha=0.1)(norm2)
-pool2 = MaxPooling2D((2, 2))(act2)
-# 104
-conv3 = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='same', use_bias=False)(pool2)
-norm3 = BatchNormalization()(conv3)
-act3 = LeakyReLU(alpha=0.1)(norm3)
-pool3 = MaxPooling2D((2, 2))(act3)
-# 52
-conv4 = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='same', use_bias=False)(pool3)
-norm4 = BatchNormalization()(conv4)
-act4 = LeakyReLU(alpha=0.1)(norm4)
-pool4 = MaxPooling2D((2, 2))(act4)
-# 26
-conv5 = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='same', use_bias=False)(pool4)
-norm5 = BatchNormalization()(conv5)
-act5 = LeakyReLU(alpha=0.1)(norm5)
-pool5 = MaxPooling2D((2, 2))(act5)
-
-conv6 = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='same', use_bias=False)(pool5)
-norm6 = BatchNormalization()(conv6)
-act6 = LeakyReLU(alpha=0.1)(norm6)
-
-conv7 = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='same', use_bias=False)(act6)
-norm7 = BatchNormalization()(conv7)
-act7 = LeakyReLU(alpha=0.1)(norm7)
-
-conv8 = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='same', use_bias=False)(act7)
-norm8 = BatchNormalization()(conv8)
-act8 = LeakyReLU(alpha=0.1)(norm8)
-
-conv9 = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='same', use_bias=False)(act8)
-norm9 = BatchNormalization()(conv9)
-act8 = LeakyReLU(alpha=0.1)(norm9)
-
-final = Conv2D(5 * (4 + 1), kernel_size=(1, 1), strides=(1, 1))(
-    act8)
-reshape = Reshape((13 * 13 * 5, 4 + 1))(final)
-out = Lambda(model.net.net2y, (13 * 13 * 5, 4 + 1))(reshape)
-
-net_model = Model(input, out)
-net_model.load_weights(in_file)
-# net_model = Model(input,pool2)
+net_model = model.net.backend
 
 netin = tf.placeholder(name="img", dtype=tf.float32, shape=model.input_shape)
 
