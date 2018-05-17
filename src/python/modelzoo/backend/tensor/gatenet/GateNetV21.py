c60195cadc55fca08c3c4bbcefa1e0c8bdda3d8f
phildue <phild@protonmail.com>
2018-05-17 08:51:36 +0200

    added gatev21 with fused pooling

--- src/python/modelzoo/backend/tensor/gatenet/GateNetV21.py
+++ src/python/modelzoo/backend/tensor/gatenet/GateNetV21.py
@@ -3,11 +3,12 @@ from keras import Input, Model
 from keras.layers import Conv2D, BatchNormalization, MaxPooling2D, LeakyReLU, Reshape, Lambda
 from keras.optimizers import Adam
 
+from modelzoo.backend.tensor.FusedBatchNormalization import FusedBatchNormalization
 from modelzoo.backend.tensor.metrics import Loss
 from modelzoo.models.Net import Net
 
 
-class GateNetV10(Net):
+class GateNetV21(Net):
 
     def compile(self, params=None, metrics=None):
         # default_sgd = SGD(lr=0.001, decay=0.0005, momentum=0.9)
@@ -59,43 +60,43 @@ class GateNetV10(Net):
                         'decay': 0.0005}
 
         w, h = img_shape
-        # increased kernel size lower level maps
+        # like v10 but fused batch norm
         input = Input((w, h, 3))
         conv1 = Conv2D(16, kernel_size=(6, 6), strides=(1, 1), padding='same', use_bias=False)(input)
-        norm1 = BatchNormalization()(conv1)
+        norm1 = FusedBatchNormalization()(conv1)
         act1 = LeakyReLU(alpha=0.1)(norm1)
         pool1 = MaxPooling2D((2, 2))(act1)
         # 208
         conv2 = Conv2D(32, kernel_size=(6, 6), strides=(1, 1), padding='same', use_bias=False)(pool1)
-        norm2 = BatchNormalization()(conv2)
+        norm2 = FusedBatchNormalization()(conv2)
         act2 = LeakyReLU(alpha=0.1)(norm2)
         pool2 = MaxPooling2D((2, 2))(act2)
         # 104
         conv3 = Conv2D(64, kernel_size=(6, 6), strides=(1, 1), padding='same', use_bias=False)(pool2)
-        norm3 = BatchNormalization()(conv3)
+        norm3 = FusedBatchNormalization()(conv3)
         act3 = LeakyReLU(alpha=0.1)(norm3)
         pool3 = MaxPooling2D((2, 2))(act3)
         # 52
         conv4 = Conv2D(64, kernel_size=(6, 6), strides=(1, 1), padding='same', use_bias=False)(pool3)
-        norm4 = BatchNormalization()(conv4)
+        norm4 = FusedBatchNormalization()(conv4)
         act4 = LeakyReLU(alpha=0.1)(norm4)
         pool4 = MaxPooling2D((2, 2))(act4)
         # 26
         conv5 = Conv2D(64, kernel_size=(6, 6), strides=(1, 1), padding='same', use_bias=False)(pool4)
-        norm5 = BatchNormalization()(conv5)
+        norm5 = FusedBatchNormalization()(conv5)
         act5 = LeakyReLU(alpha=0.1)(norm5)
         pool5 = MaxPooling2D((2, 2))(act5)
         # 13
         conv6 = Conv2D(64, kernel_size=(6, 6), strides=(1, 1), padding='same', use_bias=False)(pool5)
-        norm6 = BatchNormalization()(conv6)
+        norm6 = FusedBatchNormalization()(conv6)
         act6 = LeakyReLU(alpha=0.1)(norm6)
 
         conv7 = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='same', use_bias=False)(act6)
-        norm7 = BatchNormalization()(conv7)
+        norm7 = FusedBatchNormalization()(conv7)
         act7 = LeakyReLU(alpha=0.1)(norm7)
 
         conv8 = Conv2D(64, kernel_size=(3, 3), strides=(1, 1), padding='same', use_bias=False)(act7)
-        norm8 = BatchNormalization()(conv8)
+        norm8 = FusedBatchNormalization()(conv8)
         act8 = LeakyReLU(alpha=0.1)(norm8)
 
         final = Conv2D(n_boxes * (n_polygon + 1), kernel_size=(1, 1), strides=(1, 1))(
