5a096f1ca55218c04f797bdd2dbe250763a929ea
phild <phild@protonmail.com>
2018-05-17 13:54:57 +0200

    added exp operation

--- src/cpp/main.cpp
+++ src/cpp/main.cpp
@@ -1,27 +1,11 @@
-/* Copyright 2018 The TensorFlow Authors. All Rights Reserved.
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-    http://www.apache.org/licenses/LICENSE-2.0
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-==============================================================================*/
+//based on tensorflow/contrib/lite/examples/minimal/minimal.cc
+
 #include "tensorflow/contrib/lite/model.h"
 #include "tensorflow/contrib/lite/interpreter.h"
 #include "tensorflow/contrib/lite/kernels/register.h"
+#include "tensorflow/contrib/lite/kernels/kernel_util.h"
 #include <cstdio>
-
-// This is an example that is minimal to read a model
-// from disk and perform inference. There is no data being loaded
-// that is up to you to add as a user.
-//
-// NOTE: Do not add any dependencies to this that cannot be built with
-// the minimal makefile. This example must remain trivial to build with
-// the minimal build tool.
-//
+#include "exp.h"
 // Usage: minimal <tflite model>
 
 using namespace tflite;
@@ -31,51 +15,6 @@ using namespace tflite;
     fprintf(stderr, "Error at %s:%d\n",  __FILE__, __LINE__); \
     exit(1); \
   }
-
-
-TfLiteStatus SinPrepare(TfLiteContext* context, TfLiteNode* node) {
-  using namespace tflite;
-  TF_LITE_ENSURE_EQ(context, NumInputs(node), 1);
-  TF_LITE_ENSURE_EQ(context, NumOutputs(node), 1);
-
-  const TfLiteTensor* input = GetInput(context, node, 0);
-  TfLiteTensor* output = GetOutput(context, node, 0);
-
-  int num_dims = NumDimensions(input);
-
-  TfLiteIntArray* output_size = TfLiteIntArrayCreate(num_dims);
-  for (int i=0; i<num_dims; ++i) {
-    output_size->data[i] = input->dims->data[i];
-  }
-
-  return context->ResizeTensor(context, output, output_size);
-}
-
-TfLiteStatus SinEval(TfLiteContext* context, TfLiteNode* node) {
-  using namespace tflite;
-  const TfLiteTensor* input = GetInput(context, node,0);
-  TfLiteTensor* output = GetOutput(context, node,0);
-
-  float* input_data = input->data.f;
-  float* output_data = output->data.f;
-
-  size_t count = 1;
-  int num_dims = NumDimensions(input);
-  for (int i = 0; i < num_dims; ++i) {
-    count *= input->dims->data[i];
-  }
-
-  for (size_t i=0; i<count; ++i) {
-    output_data[i] = sin(input_data[i]);
-  }
-  return kTfLiteOk;
-}
-
-TfLiteRegistration* Register_SIN() {
-  static TfLiteRegistration r = {nullptr, nullptr, SinPrepare, SinEval};
-  return &r;
-}
-
 
 
 int main(int argc, char *argv[]) {
@@ -92,7 +31,7 @@ int main(int argc, char *argv[]) {
 
   // Build the interpreter
   tflite::ops::builtin::BuiltinOpResolver resolver;
-  resolver.AddCustom("Exp", Register_SIN());
+  resolver.AddCustom("Exp", Register_EXP());
 
   InterpreterBuilder builder(*model.get(), resolver);
   std::unique_ptr<Interpreter> interpreter;
