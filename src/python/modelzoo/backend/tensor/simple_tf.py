2619185ef6323357e0fd12f2acd653a0965b4bc0
phild <phild@protonmail.com>
2018-06-02 13:19:25 +0200

    added results of perf measurements

--- src/python/modelzoo/backend/tensor/simple_tf.py
+++ src/python/modelzoo/backend/tensor/simple_tf.py
@@ -13,11 +13,11 @@ tf.contrib.lite.subprocess = subprocess
 cd_work()
 
 
-def conv_pool_norm(input):
+def conv_pool_norm(input, filters, kernel_size):
     conv = tf.layers.conv2d(
         inputs=input,
-        filters=32,
-        kernel_size=[6, 6],
+        filters=filters,
+        kernel_size=kernel_size,
         padding='same',
         activation=tf.nn.relu,
         use_bias=False,
@@ -35,10 +35,10 @@ def conv_pool_norm(input):
     return norm
 
 
-def conv_norm(input, kernel_size):
+def conv_norm(input, filters, kernel_size):
     conv = tf.layers.conv2d(
         inputs=input,
-        filters=32,
+        filters=filters,
         kernel_size=kernel_size,
         padding='same',
         activation=tf.nn.relu,
@@ -52,31 +52,71 @@ def conv_norm(input, kernel_size):
     return norm
 
 
-if __name__ == '__main__':
-
-    input_layer = tf.placeholder(tf.float32, [1, 416, 416, 3], name='Image')
-    quantize = False
-    out_name = 'build/test'
-
-    layer1 = conv_pool_norm(input_layer)
-    layer2 = conv_pool_norm(layer1)
-    layer3 = conv_pool_norm(layer2)
-    layer4 = conv_pool_norm(layer3)
-    layer5 = conv_pool_norm(layer4)
-    layer6 = conv_norm(layer5, [6, 6])
-    layer7 = conv_norm(layer6, [3, 3])
-    layer8 = conv_norm(layer7, [3, 3])
-
-    predictions = tf.layers.conv2d(
-        layer8,
-        filters=5 * 5,
-        kernel_size=[1, 1],
-        strides=1,
-        activation=None,  # linear
+def conv(input, filters, kernel_size):
+    conv = tf.layers.conv2d(
+        inputs=input,
+        filters=filters,
+        kernel_size=kernel_size,
+        padding='same',
+        activation=tf.nn.relu,
         use_bias=False,
     )
+    return conv
 
-    netout = [tf.identity(predictions, name='Predictions')]
+
+def norm(input):
+    norm = tf.layers.batch_normalization(
+        inputs=input
+    )
+    return norm
+
+
+def pool(input):
+    pool = tf.layers.max_pooling2d(
+        input,
+        pool_size=[2, 2],
+        strides=2,
+    )
+    return pool
+
+
+def conv_norm(input, filters, kernel_size):
+    con = conv(input, filters, kernel_size)
+    return norm(con)
+
+
+if __name__ == '__main__':
+
+    input_res = 128, 128
+    n_filter = 16
+    n_layers = 1
+    input_layer = tf.placeholder(tf.float32, [1, input_res[0], input_res[1], 3], name='Image')
+    quantize = False
+    out_name = 'lib/gatenet-jevois/gatenet/share/gatenet/{}xconv_w{}_3x3-norm-{}x{}'.format(n_layers, n_filter,
+                                                                                                    input_res[0],
+                                                                                                    input_res[1])
+    layer0 = input_layer
+    for i in range(n_layers):
+        layer1 = conv_norm(layer0, n_filter, (3, 3))
+        layer0 = layer1
+    # layer2 = conv_pool_norm(layer1)
+    # layer3 = conv_pool_norm(layer2)
+    # layer4 = conv_pool_norm(layer3)
+    # layer5 = conv_pool_norm(layer4)
+    # layer6 = conv_norm(layer5, [6, 6])
+    # layer7 = conv_norm(layer6, [3, 3])
+    # layer8 = conv_norm(layer7, [3, 3])
+    #
+    # predictions = tf.layers.conv2d(
+    #     layer8,
+    #     filters=5 * 5,
+    #     kernel_size=[1, 1],
+    #     strides=1,
+    #     activation=None,  # linear
+    #     use_bias=False,
+    # )
+
+    netout = [tf.identity(layer1, name='Predictions')]
     netout_names = ['Predictions']
     init = tf.global_variables_initializer()
 
