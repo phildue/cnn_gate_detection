05f5d40501020f38c43e7f0dd87bb8c292197b6b
phildue <phild@protonmail.com>
2018-06-20 13:00:21 +0200

    final conv layer has not many filters

--- src/python/run/experiments/cropnet_layers.py
+++ src/python/run/experiments/cropnet_layers.py
@@ -1,15 +1,16 @@
 # How many layers do we need to label the grid accurately?
 from run.training.cropnet.train import train
 
-baseline = [{'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 16, 'strides': (1, 1), 'alpha': 0.1},
+baseline = [{'name': 'conv_leaky', 'kernel_size': (6, 6), 'filters': 16, 'strides': (1, 1), 'alpha': 0.1},
             {'name': 'max_pool', 'size': (2, 2)},
-            {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 16, 'strides': (1, 1), 'alpha': 0.1},
-            {'name': 'max_pool', 'size': (2, 2)},
-            {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 16, 'strides': (1, 1), 'alpha': 0.1}]
+            {'name': 'conv_leaky', 'kernel_size': (6, 6), 'filters': 16, 'strides': (1, 1), 'alpha': 0.1},
+            {'name': 'max_pool', 'size': (2, 2)}]
 
-for i in range(7):
+for i in range(1, 8):
     architecture = baseline.copy()
-    for j in range(i):
+    for j in range(i - 1):
         architecture.append(
-            {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 16, 'strides': (1, 1), 'alpha': 0.1})
+            {'name': 'conv_leaky', 'kernel_size': (6, 6), 'filters': 16, 'strides': (1, 1), 'alpha': 0.1})
+    architecture.append(
+        {'name': 'conv_leaky', 'kernel_size': (6, 6), 'filters': 4, 'strides': (1, 1), 'alpha': 0.1})
     train(architecture=architecture, work_dir='cropnet52x52-{}layers'.format(i + 3))
