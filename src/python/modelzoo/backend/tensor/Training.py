c2ffee689504e5c897da9dbe1c537cdf6c268214
phild <phild@protonmail.com>
2018-06-20 20:07:14 +0200

    reduced early stopping

--- src/python/modelzoo/backend/tensor/Training.py
+++ src/python/modelzoo/backend/tensor/Training.py
@@ -34,9 +34,9 @@ class Training:
                                        verbose=1)
             self.callbacks.append(early_stop)
         if out_file is not None:
-            checkpoint = ModelCheckpoint(log_dir + out_file, monitor='val_average_precision', verbose=2,
+            checkpoint = ModelCheckpoint(log_dir + out_file, monitor='val_loss', verbose=2,
                                          save_best_only=True,
-                                         mode='max', save_weights_only=False,
+                                         mode='min', save_weights_only=False,
                                          period=1)
             self.callbacks.append(checkpoint)
         if log_dir is not None:
