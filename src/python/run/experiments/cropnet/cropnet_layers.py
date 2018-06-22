96f4d23f6c7b94fd1bca56e76226ccab935886be
phild <phild@protonmail.com>
2018-06-22 05:36:05 +0200

    extended name fixed path

--- src/python/run/experiments/cropnet/cropnet_layers.py
+++ src/python/run/experiments/cropnet/cropnet_layers.py
@@ -15,8 +15,8 @@ parser.add_argument("--x_filter_end", help="16*x_filter per layer end",
 
 args = parser.parse_args()
 h, w = args.h, args.h
-for x in range(args.x_filter_start,args.x_filter_end):
-    baseline = [{'name': 'conv_leaky', 'kernel_size': (6, 6), 'filters': 16*x, 'strides': (1, 1), 'alpha': 0.1},
+for x in range(args.x_filter_start, args.x_filter_end):
+    baseline = [{'name': 'conv_leaky', 'kernel_size': (6, 6), 'filters': 16 * x, 'strides': (1, 1), 'alpha': 0.1},
                 {'name': 'max_pool', 'size': (2, 2)}]
 
     for i in range(1, 8):
@@ -24,7 +24,7 @@ for x in range(args.x_filter_start,args.x_filter_end):
 
         for j in range(i - 1):
             architecture.append(
-                {'name': 'conv_leaky', 'kernel_size': (6, 6), 'filters': 16*x, 'strides': (1, 1), 'alpha': 0.1})
+                {'name': 'conv_leaky', 'kernel_size': (6, 6), 'filters': 16 * x, 'strides': (1, 1), 'alpha': 0.1})
             if len(architecture) == 3:
                 architecture.append({'name': 'max_pool', 'size': (2, 2)})
 
@@ -33,4 +33,5 @@ for x in range(args.x_filter_start,args.x_filter_end):
 
         architecture.append(
             {'name': 'conv_leaky', 'kernel_size': (6, 6), 'filters': 4, 'strides': (1, 1), 'alpha': 0.1})
-        train(architecture=architecture, work_dir='cropnet{}x{}-{}layers'.format(h, w, i + 1), epochs=50, img_res=(h, w))
+        train(architecture=architecture, work_dir='cropnet{}x{}-{}layers-{}filters'.format(h, w, i + 1, 16 * x),
+              epochs=50, img_res=(h, w))
