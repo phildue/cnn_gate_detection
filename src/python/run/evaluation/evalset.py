05860897cd8dd353d9ab0733bfccc19ad4362102
phild <phild@protonmail.com>
2018-06-26 11:42:45 +0200

    prepared eval

--- src/python/run/evaluation/evalset.py
+++ src/python/run/evaluation/evalset.py
@@ -13,12 +13,13 @@ name = 'industrial'
 
 # Image Source
 batch_size = 8
-n_batches = int(500 / batch_size)
+n_batches = int(100 / batch_size)
 image_source = ['resource/ext/samples/industrial_new_test/']
 color_format = 'bgr'
 
 # Model
 model_src = 'out/refnet52x52->3x3+4layers+32filters/'
+conf_thresh = 0
 summary = load_file(model_src + 'summary.pkl')
 architecture = summary['architecture']
 img_res = 52, 52
@@ -32,18 +33,19 @@ model = GateNet.create_by_arch(norm=img_res, architecture=architecture,
                                                   ]]),
                                batch_size=batch_size,
                                color_format='yuv',
+                               conf_thresh=conf_thresh,
                                augmenter=None,
                                weight_file=model_src + 'model.h5'
                                )
 
 # Evaluator
-iou_thresh = 0.4
+iou_thresh = 0.8
 
 # Result Paths
 result_path = model_src
-result_file = 'result_' + name
+result_file = 'result_' + name + str(iou_thresh)
 result_img_path = result_path + 'images_' + name + '/'
-exp_param_file = 'experiment_parameters_' + name + '.txt'
+exp_param_file = 'experiment_parameters_' + name + str(iou_thresh) + '.txt'
 
 create_dirs([result_path, result_img_path])
 generator = CropGenerator(GateGenerator(directories=image_source, batch_size=batch_size, img_format='jpg',
@@ -51,7 +53,8 @@ generator = CropGenerator(GateGenerator(directories=image_source, batch_size=bat
 
 evaluate_generator(model, generator, n_batches=n_batches, verbose=True, out_file_labels=result_path + result_file)
 
-evaluate_file(model, result_path + result_file + '.pkl', metrics=[MetricDetection(show_=False)], verbose=True,
+evaluate_file(model, result_path + result_file + '.pkl', metrics=[MetricDetection(iou_thresh=iou_thresh, show_=False)],
+              verbose=True,
               out_file_metric=result_path + result_file + '.pkl')
 
 exp_params = {'name': name,
@@ -59,7 +62,6 @@ exp_params = {'name': name,
               'train_samples': '10k',
               'conf_thresh': conf_thresh,
               'iou_thresh': iou_thresh,
-              'weight_file': weight_file,
               'image_source': image_source,
               'color_format': color_format,
               'n_samples': n_batches * batch_size}
