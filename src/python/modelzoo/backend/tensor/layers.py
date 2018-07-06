35244e849fa0c7d44302ed56a24380f09bff052c
phildue <phild@protonmail.com>
2018-07-06 09:16:33 +0200

    v10 sep experiment

--- src/python/modelzoo/backend/tensor/layers.py
+++ src/python/modelzoo/backend/tensor/layers.py
@@ -1,4 +1,4 @@
-from keras.layers import Conv2D, BatchNormalization, LeakyReLU, MaxPooling2D, TimeDistributed
+from keras.layers import Conv2D, BatchNormalization, LeakyReLU, MaxPooling2D, TimeDistributed, SeparableConv2D
 
 
 def create_layer(netin, config):
@@ -44,7 +44,19 @@ def time_dist_max_pool(netin, size):
     return TimeDistributed(MaxPooling2D(size))(netin)
 
 
+def sep_conv_leaky_creator(netin, config):
+    return conv_leaky(netin, config['filters'], config['kernel_size'], config['strides'], config['alpha'])
+
+
+def sep_conv_leaky(netin, filters, kernel_size, strides, alpha):
+    conv = SeparableConv2D(filters, kernel_size=kernel_size, strides=strides, padding='same', use_bias=False)(netin)
+    norm = BatchNormalization()(conv)
+    act = LeakyReLU(alpha=alpha)(norm)
+    return act
+
+
 layers = {'conv_leaky': conv_leaky_creator,
+          'sep_conv_leaky': sep_conv_leaky_creator,
           'max_pool': max_pool_creator,
           'time_dist_conv_leaky': time_dist_conv_leaky_creator,
           'time_dist_max_pool': time_dist_max_pool_creator}
