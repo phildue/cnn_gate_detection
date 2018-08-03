9f3080f12ed2ab7a5ab049cb9ae414d86fb5f74c
phildue <phild@protonmail.com>
2018-08-03 17:10:57 +0200

    added dronerace as submodule, added report

---
+++ src/python/run/experiments/0108/test.py
@@ -0,0 +1,57 @@
+from modelzoo.evaluation.evalmetric import evalmetric
+from modelzoo.evaluation.evalset import evalset
+from utils.imageprocessing.transform.TransfromGray import TransformGray
+from utils.workdir import cd_work
+
+cd_work()
+
+work_dir = 'out/0108/'
+
+models = [
+    'baseline_multiscale208x208/',
+    'baseline_multiscale416x416/',
+    'multiscale208x208/',
+    'multiscale2208x208/',
+          ]
+
+for model in models:
+    preprocessing = None if 'gray' not in model else TransformGray()
+    evalset(name='rf',
+            result_path=work_dir + model + '/test/',
+            result_file='daylight_industrial.pkl',
+            batch_size=24,
+            # n_samples=48,
+            model_src=work_dir + model,
+            preprocessing=None,
+            color_format='yuv',
+            image_source=['resource/ext/samples/daylight_test/', 'resource/ext/samples/industrial_new_test/'])
+
+box_range = [0.0, 1.0]
+for model in models:
+    for iou_thresh in [0.4, 0.6]:
+        for i in range(len(box_range) - 1):
+            evalmetric(name='total_iou{}'.format(iou_thresh, box_range[i]),
+                       min_box_area=box_range[i],
+                       max_box_area=box_range[i + 1],
+                       iou_thresh=iou_thresh,
+                       batch_size=24,
+                       model_src=work_dir + model,
+                       color_format='yuv',
+                       label_file=work_dir + model + '/test/daylight_industrial.pkl',
+                       result_path=work_dir + model + '/test/',
+                       show=False)
+
+box_range = [0.01, 0.05, 0.1, 0.15, 0.25, 0.5, 1.0]
+for model in models:
+    for iou_thresh in [0.4, 0.6]:
+        for i in range(len(box_range) - 1):
+            evalmetric(name='total_iou{}_range{}-{}'.format(iou_thresh, box_range[i], box_range[i + 1]),
+                       min_box_area=box_range[i],
+                       max_box_area=box_range[i + 1],
+                       iou_thresh=iou_thresh,
+                       batch_size=24,
+                       model_src=work_dir + model,
+                       color_format='yuv',
+                       label_file=work_dir + model + '/test/daylight_industrial.pkl',
+                       result_path=work_dir + model + '/test/',
+                       show=False)
