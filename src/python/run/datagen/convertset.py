9f103d6de62f98feec3d0477d14c100844ee79c3
phild <phild@protonmail.com>
2018-08-27 18:06:29 +0200

    modified convertset for flight

--- src/python/run/datagen/convertset.py
+++ src/python/run/datagen/convertset.py
@@ -3,15 +3,32 @@ from utils.fileaccess.utils import create_dirs
 from utils.workdir import cd_work
 
 cd_work()
-reader = DatasetParser.get_parser(directory='resource/ext/samples/industrial_new/',
-                                  label_format='xml',
-                                  color_format='bgr',
-                                  image_format='jpg'
-                                  )
-writer = DatasetParser.get_parser(directory='resource/ext/samples/set_yolo/',
-                                  label_format='yolo',
-                                  color_format='bgr')
+src_dir = 'resource/ext/samples/'
+target_dir = 'resource/ext/samples/yolo/flight_bgr/'
 
-create_dirs(['resource/ext/samples/set_yolo/'])
-images, labels = reader.read(100)
-writer.write(images, labels)
+sets = [src_dir + 'daylight_course1',
+        src_dir + 'daylight_course5',
+        src_dir + 'daylight_course3',
+        src_dir + 'iros2018_course1',
+        src_dir + 'iros2018_course5',
+        src_dir + 'basement_course3',
+        src_dir + 'basement_course1',
+        src_dir + 'iros2018_course3_test']
+
+start_idx = 0
+create_dirs([target_dir])
+for dataset in sets:
+    reader = DatasetParser.get_parser(directory=src_dir + dataset,
+                                      label_format='xml',
+                                      color_format='bgr',
+                                      image_format='jpg'
+                                      )
+    writer = DatasetParser.get_parser(directory=target_dir,
+                                      label_format='yolo',
+                                      color_format='bgr',
+                                      start_idx=start_idx)
+
+    images, labels = reader.read(10)
+    writer.write(images, labels)
+
+    start_idx += len(images)
