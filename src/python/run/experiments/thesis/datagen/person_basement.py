413b9244a9dc3b8f9d197ebc3962faaa257feda8
phildue <phild@protonmail.com>
2018-09-03 16:57:36 +0200

    prep person_basement

--- src/python/run/experiments/thesis/datagen/person_basement.py
+++ src/python/run/experiments/thesis/datagen/person_basement.py
@@ -1,61 +1,67 @@
+import argparse
+
 import numpy as np
 
 from training.yolo.train import train
 
-n_repetitions = 10
-img_res = 416, 416
-# anchors = np.array([[[2.53, 2.54],
-#                      [4.21, 5.28],
-#                      [10.75, 9.96]],
-#                     [[0.62, 0.86],
-#                      [1.4, 1.9],
-#                      [2.31, 3.625],
-#                      ]])
-anchors = np.array([
-    [[10, 14],
-     [23, 27],
-     [37, 58]],
-    [[81, 82],
-     [135, 169],
-     [344, 319]],
-])
-architecture = [
-    {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 16, 'strides': (1, 1), 'alpha': 0.1},
-    {'name': 'max_pool', 'size': (2, 2)},
-    {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 64, 'strides': (1, 1), 'alpha': 0.1},
-    {'name': 'max_pool', 'size': (2, 2)},
-    {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 128, 'strides': (1, 1), 'alpha': 0.1},
-    {'name': 'max_pool', 'size': (2, 2)},
-    {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 256, 'strides': (1, 1), 'alpha': 0.1},
-    {'name': 'max_pool', 'size': (2, 2)},
-    {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 512, 'strides': (1, 1), 'alpha': 0.1},
-    {'name': 'max_pool', 'size': (2, 2)},
-    {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 1024, 'strides': (1, 1), 'alpha': 0.1},
-    {'name': 'conv_leaky', 'kernel_size': (1, 1), 'filters': 256, 'strides': (1, 1), 'alpha': 0.1},
-    {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 512, 'strides': (1, 1), 'alpha': 0.1},
-    {'name': 'predict'},
-    {'name': 'route', 'index': [-4]},
-    {'name': 'conv_leaky', 'kernel_size': (1, 1), 'filters': 128, 'strides': (1, 1), 'alpha': 0.1},
-    {'name': 'upsample', 'size': 2},
-    {'name': 'route', 'index': [-1, 8]},
-    {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 256, 'strides': (1, 1), 'alpha': 0.1},
-    {'name': 'predict'}
-]
+if __name__ == '__main__':
+    start_idx = 0
+    n_repetitions = 5
+    img_res = 416, 416
 
-model_name = 'yolov3_person{}x{}'.format(img_res[0], img_res[1])
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--start_idx", default=start_idx, type=int)
+    parser.add_argument("--n_reps", default=n_repetitions, type=int)
 
-augmenter = None
+    args = parser.parse_args()
 
-image_source = ['resource/ext/samples/muro']
+    start_idx = args.start_idx
+    n_repetitions = args.n_reps
+    anchors = np.array([
+        [[10, 14],
+         [23, 27],
+         [37, 58]],
+        [[81, 82],
+         [135, 169],
+         [344, 319]],
+    ])
+    architecture = [
+        {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 16, 'strides': (1, 1), 'alpha': 0.1},
+        {'name': 'max_pool', 'size': (2, 2)},
+        {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 64, 'strides': (1, 1), 'alpha': 0.1},
+        {'name': 'max_pool', 'size': (2, 2)},
+        {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 128, 'strides': (1, 1), 'alpha': 0.1},
+        {'name': 'max_pool', 'size': (2, 2)},
+        {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 256, 'strides': (1, 1), 'alpha': 0.1},
+        {'name': 'max_pool', 'size': (2, 2)},
+        {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 512, 'strides': (1, 1), 'alpha': 0.1},
+        {'name': 'max_pool', 'size': (2, 2)},
+        {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 1024, 'strides': (1, 1), 'alpha': 0.1},
+        {'name': 'conv_leaky', 'kernel_size': (1, 1), 'filters': 256, 'strides': (1, 1), 'alpha': 0.1},
+        {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 512, 'strides': (1, 1), 'alpha': 0.1},
+        {'name': 'predict'},
+        {'name': 'route', 'index': [-4]},
+        {'name': 'conv_leaky', 'kernel_size': (1, 1), 'filters': 128, 'strides': (1, 1), 'alpha': 0.1},
+        {'name': 'upsample', 'size': 2},
+        {'name': 'route', 'index': [-1, 8]},
+        {'name': 'conv_leaky', 'kernel_size': (3, 3), 'filters': 256, 'strides': (1, 1), 'alpha': 0.1},
+        {'name': 'predict'}
+    ]
 
-for i in range(n_repetitions):
-    train(architecture=architecture,
-          work_dir='thesis/datagen' + model_name + '_i{0:02d}'.format(i),
-          img_res=img_res,
-          augmenter=augmenter,
-          image_source=image_source,
-          anchors=anchors,
-          epochs=100,
-          n_samples=None,
-          class_names='muro',
-          initial_epoch=0)
+    model_name = 'yolov3_person{}x{}'.format(img_res[0], img_res[1])
+
+    augmenter = None
+
+    image_source = ['resource/ext/samples/muro']
+
+    for i in range(start_idx, n_repetitions):
+        train(architecture=architecture,
+              work_dir='thesis/datagen' + model_name + '_i{0:02d}'.format(i),
+              img_res=img_res,
+              augmenter=augmenter,
+              image_source=image_source,
+              anchors=anchors,
+              epochs=100,
+              n_samples=None,
+              class_names='muro',
+              initial_epoch=0)
