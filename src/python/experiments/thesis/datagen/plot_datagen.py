a79b7a8b2f668e915e50ee3a682cf066f1cf58fa
phildue <phild@protonmail.com>
2018-10-18 22:26:19 +0200

    update 40k

--- src/python/experiments/thesis/datagen/plot_datagen.py
+++ src/python/experiments/thesis/datagen/plot_datagen.py
@@ -33,8 +33,32 @@ names = [
     'Chromatic',
     'Exposure'
 ]
+
+frame = pd.DataFrame()
+frame['Name'] = pd.Series(names)
+
+'''
+Validation mAP
+'''
+validation_map = []
+for m, model in enumerate(models):
+    val_aps = []
+    for i in range(n_iterations):
+        model_dir = model + '_i0{}'.format(i)
+        log_file = work_dir + model_dir + '/log.csv'
+        history = load_file(log_file)
+        val_ap = history[-1][-2]
+
+        val_aps.append(float(val_ap))
+    validation_map.append((np.round(np.mean(val_aps), 2), np.round(np.std(val_aps), 2)))
+
+frame['Validation mAP'] = pd.Series(validation_map)
+
+'''
+Test on Simulated Data
+'''
 testset = 'iros2018_course_final_simple_17gates'
-iou_thresh = 0.6
+iou_thresh = [0.4, 0.6, 0.8]
 plt.figure(figsize=(8, 3))
 plt.title('Precision - Recall IoU:{}'.format(iou_thresh))
 plt.subplot(1, 2, 1)
@@ -42,30 +66,33 @@ plt.xlabel('Recall')
 plt.ylabel('Precision')
 plt.title("Results in Virtual Environment")
 plt.ylim(0.0, 1.1)
-frame = pd.DataFrame()
-frame['Name'] = pd.Series(names)
-results_on_sim = []
-for m, model in enumerate(models):
-    total_detections = []
-    mean_detections = []
-    for i in range(n_iterations):
-        model_dir = model + '_i0{}'.format(i)
-        result_file = work_dir + model_dir + '/test_' + testset + '/' + 'results_iou{}.pkl'.format(iou_thresh)
-        if "snake" in model:
-            result_file = 'out/thesis/snake/test_{}_results_iou{}_{}.pkl'.format(testset, iou_thresh, i)
-        try:
-            results = load_file(result_file)
-            total_detections.append(sum_results(results['results']))
-        except FileNotFoundError:
-            continue
 
-    m_p, m_r, std_p, std_R = average_precision_recall(total_detections)
-    meanAp = np.mean(m_p)
-    errAp = np.mean(std_p)
-    results_on_sim.append(meanAp)#, errAp
-    plt.plot(m_r, m_p, 'x--')
-frame['Sim Data'] = pd.Series(results_on_sim)
+for iou in iou_thresh:
+    results_on_sim = []
+    for m, model in enumerate(models):
+        total_detections = []
+        mean_detections = []
+        for i in range(n_iterations):
+            model_dir = model + '_i0{}'.format(i)
+            result_file = work_dir + model_dir + '/test_' + testset + '/' + 'results_iou{}.pkl'.format(iou)
+            if "snake" in model:
+                result_file = 'out/thesis/snake/test_{}_results_iou{}_{}.pkl'.format(testset, iou, i)
+            try:
+                results = load_file(result_file)
+                total_detections.append(sum_results(results['results']))
+            except FileNotFoundError:
+                continue
 
+        m_p, m_r, std_p, std_R = average_precision_recall(total_detections)
+        meanAp = np.mean(m_p)
+        errAp = np.mean(std_p)
+        results_on_sim.append((np.round(meanAp, 2), np.round(errAp, 2)))
+        plt.plot(m_r, m_p, 'x--')
+        frame['Sim Data' + str(iou)] = pd.Series(results_on_sim)
+
+'''
+Test on Real Data
+'''
 datasets = [
     'jevois_cyberzoo',
     'jevois_basement',
@@ -76,40 +103,43 @@ datasets_names = [
     'Basement',
     'Hallway'
 ]
-results_on_real = []
 
 plt.subplot(1, 2, 2)
 plt.title('Results on Real World Datasets'.format(iou_thresh))
 plt.xlabel('Recall')
 plt.ylabel('Precision')
 plt.ylim(0.0, 1.1)
-precision = np.zeros((3, 11))
-recall = np.zeros((3, 11))
-err_p = np.zeros((3, 11))
-for m, model in enumerate(models):
-    for j, d in enumerate(datasets):
-        total_detections = []
-        for i in range(n_iterations):
-            model_dir = model + '_i0{}'.format(i)
-            result_file = work_dir + model_dir + '/test_' + d + '/' + 'results_iou{}.pkl'.format(iou_thresh)
-            if "snake" in model:
-                result_file = work_dir + model + '{}_boxes{}-{}_iou{}_i0{}.pkl'.format(d, 0, 2.0, iou_thresh, i)
-            try:
-                results = load_file(result_file)
-                total_detections.append(sum_results(results['results']))
-            except FileNotFoundError:
-                continue
 
-        m_p, m_r, std_p, std_R = average_precision_recall(total_detections)
-        precision[j] = m_p
-        recall[j] = m_r
-        err_p[j] = std_p
-    meanAp = np.mean(precision, 0)
-    plt.plot(np.mean(recall, 0), np.mean(precision, 0), 'x--')
-    results_on_real.append(np.mean(meanAp))#, np.mean(np.mean(err_p, 0))
+for iou in iou_thresh:
+    results_on_real = []
+    precision = np.zeros((3, 11))
+    recall = np.zeros((3, 11))
+    err_p = np.zeros((3, 11))
+    for m, model in enumerate(models):
+            for j, d in enumerate(datasets):
+                total_detections = []
+                for i in range(n_iterations):
+                    model_dir = model + '_i0{}'.format(i)
+                    result_file = work_dir + model_dir + '/test_' + d + '/' + 'results_iou{}.pkl'.format(iou)
+                    if "snake" in model:
+                        result_file = work_dir + model + '{}_boxes{}-{}_iou{}_i0{}.pkl'.format(d, 0, 2.0, iou_thresh, i)
+                    try:
+                        results = load_file(result_file)
+                        total_detections.append(sum_results(results['results']))
+                    except FileNotFoundError:
+                        continue
 
-frame['Real Data'] = pd.Series(results_on_real)
+                m_p, m_r, std_p, std_R = average_precision_recall(total_detections)
+                precision[j] = m_p
+                recall[j] = m_r
+                err_p[j] = std_p
+            meanAp = np.mean(precision, 0)
+            plt.plot(np.mean(recall, 0), np.mean(precision, 0), 'x--')
+            results_on_real.append((np.round(np.mean(meanAp), 2), np.round(np.mean(np.mean(err_p, 0)), 2)))
+
+    frame['Real Data' + str(iou)] = pd.Series(results_on_real)
+
 frame.set_index('Name')
 plt.legend(names)
-print(frame)
+print(frame.to_string())
 # plt.show(True)
--- src/python/run/etc/gendistmodel.py
+++ src/python/run/etc/gendistmodel.py
@@ -1,45 +1,42 @@
-from utils.fileaccess.GateGenerator import GateGenerator
+from utils.fileaccess.utils import load_file
 from utils.imageprocessing.BarrelDistortion import BarrelDistortion
-from utils.imageprocessing.Imageprocessing import show
-from utils.timing import tic
 from utils.workdir import cd_work
 
 cd_work()
-dataset = GateGenerator(directories=['resource/ext/samples/daylight_course1/'],
-                        batch_size=100, color_format='bgr',
-                        shuffle=False, start_idx=0, valid_frac=0,
-                        label_format='xml').generate()
-batch = next(dataset)
+# dataset = GateGenerator(directories=['resource/ext/samples/daylight_course1/'],
+#                         batch_size=100, color_format='bgr',
+#                         shuffle=False, start_idx=0, valid_frac=0,
+#                         label_format='xml').generate()
+# batch = next(dataset)
 
-# camera_calibration = load_file('resource/camera_calibration/jevois.pkl')
-#
-# distortion = camera_calibration.distortion[0]
-# k1 = distortion[0]
-# k2 = distortion[1]
-# p1 = distortion[2]
-# p2 = distortion[3]
-k1 = 0.1
-k2 = 0
-p1 = 0.1
-p2 = 0
+camera_calibration = load_file('resource/camera_calibration/jevois.pkl')
 
-distortion_model = BarrelDistortion(img_shape=(480, 640),
+distortion = camera_calibration.distortion[0]
+k1 = distortion[0]
+k2 = distortion[1]
+p1 = distortion[2]
+p2 = distortion[3]
+# k1 = 0.1
+# k2 = 0
+# p1 = 0.1
+# p2 = 0
+
+distortion_model = BarrelDistortion(img_shape=(240, 320),
                                     rad_dist_params=[k1, k2],
                                     tangential_dist_params=[p1, p2],
                                     max_iterations=100,
                                     distortion_radius=1,
-                                    conv_thresh=0.0000001,
-                                    scale=0.95)
-distortion_model.save('resource/camera_calibration/distortion_model_guess.pkl')
+                                    conv_thresh=0.0000001)
+distortion_model.save('resource/camera_calibration/distortion_model_est.pkl')
 
-distortion_model = BarrelDistortion.from_file('resource/camera_calibration/distortion_model_guess.pkl')
-for img, label, _ in batch:
-    # img, label = resize(img, (80, 166), label=label)
-
-    tic()
-    img_d, label_d = distortion_model.undistort(img, label)
-
-    # img, label = resize(img, (320, 624), label=label)
-    # img_d, label_d = resize(img_d, (320, 624), label=label_d)
-    show(img, labels=label, t=1)
-    show(img_d, labels=label_d, name='undistorted')
+# distortion_model = BarrelDistortion.from_file('resource/camera_calibration/distortion_model_est.pkl')
+# for img, label, _ in batch:
+#     img, label = resize(img, (240, 320), label=label)
+#
+#     tic()
+#     img_d, label_d = distortion_model.undistort(img, label)
+#
+#     # img, label = resize(img, (320, 624), label=label)
+#     # img_d, label_d = resize(img_d, (320, 624), label=label_d)
+#     show(img, labels=label, t=1)
+#     show(img_d, labels=label_d, name='undistorted')
