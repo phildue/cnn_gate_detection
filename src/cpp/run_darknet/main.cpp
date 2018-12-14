808901d1d6d5b1ec068bcf3b60062fb850772487
phild <phild@protonmail.com>
2018-12-14 11:14:13 +0100

    cleanup

--- src/cpp/run_darknet/main.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by phil on 16/08/18.
-//
-#include "darknet.h"
-
-int main(int argc,char *argv[]){
-
-}
--- src/python/experiments/1807/boxcount.py
+++
@@ -1,6 +0,0 @@
-from samplegen.setanalysis.SetAnalyzer import SetAnalyzer
-from utils.workdir import cd_work
-
-cd_work()
-SetAnalyzer((416, 416), ['resource/ext/samples/daylight_test/',
-                         'resource/ext/samples/industrial_new_test/']).area_distribution_hist().show()
